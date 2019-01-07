d28689cde3084c740b49ced161eb24de95c4b974
pando85 <pando855@gmail.com>
2019-01-07 09:00:03 +0100

    Add script update versions

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+NGINX_VERSION_STRING="ENV NGINX_VERSION"
+
+get_version() {
+    URL=https://raw.githubusercontent.com/nginxinc/docker-nginx/master/$1/alpine/Dockerfile
+    curl $URL 2> /dev/null | grep "$NGINX_VERSION_STRING" | cut -d' ' -f3
+}
+
+update_version() {
+    sed -i -e "s/^${NGINX_VERSION_STRING}.*/${NGINX_VERSION_STRING} $1/g" $2/alpine/Dockerfile
+}
+
+update_version $(get_version stable) stable
+update_version $(get_version mainline) mainline
