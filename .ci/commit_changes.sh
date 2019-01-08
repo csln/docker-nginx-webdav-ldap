07a1bb58a6bf5a92b9c5eeb69d74b997e7dc8e40
pando85 <pando855@gmail.com>
2019-01-08 02:29:07 +0100

    Fix: execute git commit

--- .ci/commit_changes.sh
+++ .ci/commit_changes.sh
@@ -6,7 +6,7 @@ realpath() { python -c "import os,sys; print(os.path.realpath(sys.argv[1]))" "$1
 WORKDIR=$(dirname $(realpath $0))
 COMMIT_FILES="stable/alpine/Dockerfile mainline/alpine/Dockerfile"
 
-if git diff --exit-code -- $COMMIT_FILES && [ -z "${TRAVIS_REPO_SLUG}" ]; then
+if ! git diff --exit-code -- $COMMIT_FILES && [ -z "${TRAVIS_REPO_SLUG}" ]; then
     git config user.name "travis-ci"
     git config user.email "travis-ci@travis-ci.org"
     git add $COMMIT_FILES
